ffcae8fa73892e7a1b623fd6d0af134aa7324405
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-10 16:12:47 +0100

    ADD: DeleteButton

--- components/CardBlog.jsx
+++ components/CardBlog.jsx
@@ -1,19 +1,22 @@
 import style from './CardBlog.module.css';
 import { Link } from 'react-router-dom';
+import DeletePost from './DeletePost'; // Importa il componente DeletePost
 
-export default function CardBlog({ post }) {
-    const { id,title, content, image, categories } = post;
+export default function CardBlog({ post, onDelete }) {
+    const { id, title, content, image, categories } = post;
 
     return (
-        <div style={{ display: "inline-block" }}>
+        <div className={style.cardContainer}>
             <div className={style.card}>
                 <img src={image} alt={title} />
                 <h3>{title}</h3>
                 <p>{content}</p>
                 <p>{categories}</p>
-                <Link className={style.link} to = {`/posts/${id}`}> Vai alla pagina del prodotto</Link>
+                <Link className={style.link} to={`/posts/${id}`}>Vai alla pagina del prodotto</Link>
+
+                {/* BOTTONE ELIMINA */}
+                <DeletePost id={id} onDelete={onDelete} />
             </div>
-            
         </div>
     );
 }
---
+++ src/pages/posts/Show.jsx
@@ -0,0 +1,4 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { BASE_URI } from "../config";
