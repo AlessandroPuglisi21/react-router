769000522df7b5328c0a7056db407c0a9c8d0869
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-10 14:26:43 +0100

    ADD: Error 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Prodotti from './pages/Prodotti';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Prodotti from "./pages/Prodotti";
+import NotFound from "./pages/NotFound";
+import BlankLayout from "./layouts/BlankLayout";
 
 function App() {
   return (
@@ -10,8 +12,11 @@ function App() {
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path='/about' element = {<About/>}></Route>
-          <Route path = '/prodotti' element = {<Prodotti/>}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/prodotti" element={<Prodotti />}></Route>
+        </Route>
+        <Route element={<BlankLayout />}>
+          <Route path="*" Component={NotFound}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
